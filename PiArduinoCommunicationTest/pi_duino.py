2b576d57afc131757caa82b2d15e15b0287a00ac
Laksh Bhambhani <lakshbh@gmail.com>
2019-03-02 20:46:39 -0800

    Update pi_duino.py

--- PiArduinoCommunicationTest/pi_duino.py
+++ PiArduinoCommunicationTest/pi_duino.py
@@ -1,1 +1,10 @@
-
+import serial
+import time
+ser = serial.Serial("/dev/ttyUSB0", 9600)
+ser.flushInput
+time.sleep(5)
+while 1 == 1:
+    ser.write('1')
+    time.sleep(5)
+    ser.write('2')
+    time.sleep(5)
