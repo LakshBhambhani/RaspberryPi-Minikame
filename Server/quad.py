e3fdc85031aea16f571b70391918072e2478afb5
Laksh Bhambhani <lakshbh@gmail.com>
2019-03-03 13:45:16 -0800

    Create quad.py

---
+++ Server/quad.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import serial
+
+app = Flask(__name__)
+ser = serial.Serial("/dev/ttyUSB0", 9600)
+
+ser.flushInput()
+
+# The function below is executed when someone requests a URL with the pin number and action in it:
+@app.route("/<action>")
+def action(changePin, action):
+   if action == "walkforward":
+      ser.write("1")
+      message = "Walking Forward"
+   if action == "turnLeft":
+      ser.write("2")
+      message = "Turning Left"
+   if action == "turnRight":
+      ser.write("3")
+      message = "Turning Right"
+
+   # Along with the pin dictionary, put the message into the template data dictionary:
+   templateData = {
+      'message' : message
+   }
+
+   return render_template('main.html', **templateData)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=True)
