3a3a247b409c31e034ca99aeb2e012df59750a7c
Laksh Bhambhani <lakshbh@gmail.com>
2019-03-07 13:31:00 -0800

    ios app buttons organized

--- App/IOS/Raspi Quaruped/Raspi Quaruped/ViewController.swift
+++ App/IOS/Raspi Quaruped/Raspi Quaruped/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.webView.loadRequest(URLRequest(url:URL(string: "https://www.google.com"))
         // Do any additional setup after loading the view, typically from a nib.
     }
 
