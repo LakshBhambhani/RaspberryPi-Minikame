c9dfd74f3ec8914ca23e5f9022ba589058e6d7d7
Laksh Bhambhani <lakshbh@gmail.com>
2019-03-08 12:39:18 -0800

    Update quad.py

--- Server/quad.py
+++ Server/quad.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO
 import serial
 from flask import Flask, render_template, request
-from camera import VideoCamera
 
 app = Flask(__name__)
 ser = serial.Serial("/dev/ttyUSB0", 9600)
@@ -37,17 +36,6 @@ for pin in pins:
 @app.route('/')
 def index():
     return render_template('index.html')
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/")
 def main():
