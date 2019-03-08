5c1b4a75dbf2e2636ab832a3b8ff37e470c10e12
Laksh Bhambhani <lakshbh@gmail.com>
2019-03-08 09:42:47 -0800

    updated server code to stream webcam

--- Server/quad.py
+++ Server/quad.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import serial
 from flask import Flask, render_template, request
+from camera import VideoCamera
 
 app = Flask(__name__)
 ser = serial.Serial("/dev/ttyUSB0", 9600)
@@ -32,6 +33,21 @@ pins = {
 for pin in pins:
    GPIO.setup(pin, GPIO.OUT)
    GPIO.output(pin, GPIO.LOW)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/")
 def main():
