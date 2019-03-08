96c995a62ac0a061564f9711daf0f0b629d72767
Laksh Bhambhani <lakshbh@gmail.com>
2019-03-07 22:27:25 -0800

    IOS App textbox for ip Done

--- App/IOS/Raspi Quaruped/Raspi Quaruped/ViewController.swift
+++ App/IOS/Raspi Quaruped/Raspi Quaruped/ViewController.swift
@@ -22,67 +22,90 @@ class ViewController: UIViewController, WKUIDelegate {
     @IBOutlet weak var turnRight: UIButton!
     @IBOutlet weak var bendBack: UIButton!
     
+    @IBOutlet weak var textField: UITextField!
+    
+    var text = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let url = URL(string: "https://192.168.0.36");
         let request = URLRequest(url: url!);
         webView.loadRequest(request);
+        textField.text = "192.168.0.36"
+        textField.placeholder = "IP Address of Raspberry Pi"
+        text = textField.text!
     }
 
     @IBAction func bow(_ sender: UIButton) {
-        let url = URL(string: "http://192.168.0.36/bow");
+        text = textField.text!
+        let url = URL(string: "http://" + text + "/bow");
         let request = URLRequest(url: url!);
         webView.loadRequest(request);
         print("Bowing")
+        print("IP Address: " + text)
     }
     
     @IBAction func leanLeft(_ sender: Any) {
-        let url = URL(string: "http://192.168.0.36/leanLeft");
+        text = textField.text!
+        let url = URL(string: "http://" + text + "/leanLeft");
         let request = URLRequest(url: url!);
         webView.loadRequest(request);
         print("Leaning Left")
+        print("IP Address: " + text)
     }
     
     @IBAction func walkForward(_ sender: Any) {
-        let url = URL(string: "http://192.168.0.36/walkForward");
+        text = textField.text!
+        let url = URL(string: "http://" + text + "/walkForward");
         let request = URLRequest(url: url!);
         webView.loadRequest(request);
         print("Walking Forward")
+        print("IP Address: " + text)
     }
     
     @IBAction func leanRight(_ sender: Any) {
-        let url = URL(string: "http://192.168.0.36/leanRight");
+        text = textField.text!
+        let url = URL(string: "http://" + text + "/leanRight");
         let request = URLRequest(url: url!);
         webView.loadRequest(request);
         print("Leaning Right")
+        print("IP Address: " + text)
     }
     
     @IBAction func turnLeft(_ sender: Any) {
-        let url = URL(string: "http://192.168.0.36/turnLeft");
+        text = textField.text!
+        let url = URL(string: "http://" + text + "/turnLeft");
         let request = URLRequest(url: url!);
         webView.loadRequest(request);
         print("Turning Left")
+        print("IP Address: " + text)
     }
     
     @IBAction func homePos(_ sender: Any) {
-        let url = URL(string: "http://192.168.0.36/homePos");
+        text = textField.text!
+        let url = URL(string: "http://" + text + "/homePos");
         let request = URLRequest(url: url!);
         webView.loadRequest(request);
         print("Switching to Home Position")
+        print("IP Address: " + text)
     }
     
     @IBAction func turnRight(_ sender: Any) {
-        let url = URL(string: "http://192.168.0.36/turnRight");
+        text = textField.text!
+        let url = URL(string: "http://" + text + "/turnRight");
         let request = URLRequest(url: url!);
         webView.loadRequest(request);
         print("Turning Right")
+        print("IP Address: " + text)
     }
     
     @IBAction func bendBack(_ sender: Any) {
-        let url = URL(string: "http://192.168.0.36/bendBack");
+        text = textField.text!
+        let url = URL(string: "http://" + text + "/bendBack");
         let request = URLRequest(url: url!);
         webView.loadRequest(request);
         print("Bending Back")
+        print("IP Address: " + text)
     }
 }
 
