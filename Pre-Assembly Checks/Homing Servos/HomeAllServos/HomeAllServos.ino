63bf20414b2d6136820acbc3ef874118be995a15
Laksh Bhambhani <lakshbh@gmail.com>
2019-03-09 13:28:39 -0800

    Update HomeAllServos.ino

--- Pre-Assembly Checks/Homing Servos/HomeAllServos/HomeAllServos.ino
+++ Pre-Assembly Checks/Homing Servos/HomeAllServos/HomeAllServos.ino
@@ -13,11 +13,6 @@ Servo BL_FOOT;      // Servo number: 6
 Servo BR_HIP;       // Servo number: 7
 Servo BR_FOOT;      // Servo number: 8
 
-Servo armShoulder;  // Servo number: 9
-Servo armElbow;     // Servo number: 10
-Servo armWrist;     // Servo number: 11
-Servo armFist;      // Servo number: 12
-
 void setup() {
   FL_HIP.attach(4);
   FL_FOOT.attach(5);
@@ -27,11 +22,6 @@ void setup() {
   BL_FOOT.attach(9);
   BR_HIP.attach(10);
   BR_FOOT.attach(11);
-
-  armShoulder.attach(3);
-  armElbow.attach(2);
-  armWrist.attach(1);
-  armFist.attach(0);
 }
 
 void loop() {
@@ -43,8 +33,4 @@ void loop() {
   BL_FOOT.write(70);
   BR_HIP.write(40);
   BR_FOOT.write(110);
-  armShoulder.write(160);
-  armElbow.write(0);
-  armWrist.write(90);
-  armFist.write(0);
 }
