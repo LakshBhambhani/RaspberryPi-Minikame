5fed3313815b1cc4a7b3d3d959321d719bec1cdd
Laksh Bhambhani <lakshbh@gmail.com>
2019-03-08 19:34:36 -0800

    server with camera done

--- Server/quad.py
+++ Server/quad.py
@@ -1,6 +1,9 @@
 import RPi.GPIO as GPIO
 import serial
 from flask import Flask, render_template, request
+import cv2
+import sys
+import numpy
 
 app = Flask(__name__)
 ser = serial.Serial("/dev/ttyUSB0", 9600)
@@ -36,6 +39,37 @@ for pin in pins:
 @app.route('/')
 def index():
     return render_template('index.html')
+   
+def gen():
+    i=1
+    while i<10:
+        yield (b'--frame\r\n'
+            b'Content-Type: text/plain\r\n\r\n'+str(i)+b'\r\n')
+        i+=1
+
+def get_frame():
+
+    camera_port=0
+
+    ramp_frames=100
+
+    camera=cv2.VideoCapture(camera_port) #this makes a web cam object
+
+    
+    i=1
+    while True:
+        retval, im = camera.read()
+        imgencode=cv2.imencode('.jpg',im)[1]
+        stringData=imgencode.tostring()
+        yield (b'--frame\r\n'
+            b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+        i+=1
+
+    del(camera)
+
+@app.route('/calc')
+def calc():
+     return Response(get_frame(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/")
 def main():
